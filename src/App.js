fd8b8b87a62da389bf57ff487579ad1c17586c02
Fei Sun <feisun@Feis-MacBook-Pro.local>
2025-08-05 15:52:57 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import logo from './logo.svg';
+
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import About from './About/About';
+import Home from './Home/Home';
+import Contact from './Contact/Contact'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-          Learn React
-      </header>
+    <Router>
+      <div className="nav-bar" style={{ padding: '20px' }}>
+        <nav style={{ marginBottom: '20px' }}>
+          <Link to="/" style={{ marginRight: '10px' }}>Home</Link>
+          <Link to="/about" style={{ marginRight: '10px' }}>About</Link>
+          <Link to="/contact">Contact</Link>
+        </nav>
+        </div>
+        <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
     </div>
   );
 }
